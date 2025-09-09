dbd417a2aa259024a6d8d79a58ec69e6a3374108
Maxime Moreillon <m.moreillon@gmail.com>
2025-09-09 10:41:00 +0900

    minor

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from typing import Annotated
-
 from typing_extensions import TypedDict
 
-from langgraph.graph import StateGraph, START
+from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 
 
@@ -10,18 +9,18 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-graph_builder = StateGraph(State)
-
-
 def dummy_node(state: State):
     return {"messages": state["messages"]}
 
+
+graph_builder = StateGraph(State)
 
 graph_builder.add_node("start_node", dummy_node)
 graph_builder.add_node("second_node", dummy_node)
 
 graph_builder.add_edge(START, "start_node")
 graph_builder.add_edge("start_node", "second_node")
+graph_builder.add_edge("start_node", END)
 
 graph = graph_builder.compile()
 
@@ -30,4 +29,4 @@ print(graph.get_graph().draw_ascii())
 
 
 response = graph.invoke({"messages": ["message 1", "message 2"]})
-print(response)
+print(response["messages"])
